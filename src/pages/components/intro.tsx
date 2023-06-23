c53d91d5b5b7ce352f26bb03b9bd1b7f6e92e5b3
JoaoJunqueira <vitjunqueira38@gmail.com>
2023-06-23 17:37:09 -0300

    feat: texto intro (section)

---
+++ src/pages/components/intro.tsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+    return (
+      <section>
+        <div>WEBNARS EXCLUSIVOS</div>
+        <h3>Menos Conversinha,</h3>
+        <h1>Mais Conversão</h1>
+      </section>
+    );
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header"
+import Intro from "./components/intro"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Intro />
       <h1>Hello World</h1>
     </div>
   )
